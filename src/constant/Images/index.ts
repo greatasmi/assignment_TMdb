f59a2d535845bf124f513e9f13ffcb547264c9b0
Asma Arain <greatasma@yahoo.com>
2025-10-24 20:48:04 -0700

    constant for define images

---
+++ src/constant/Images/index.ts
@@ -0,0 +1,16 @@
+import  search from '../../assets/images/search.png';
+import movie_poster from '../../assets/icons/movie_poster.png';
+import playbutton from '../../assets/icons/playbutton.png';
+import searchsvg from '../../assets/icons/search.svg';
+
+
+const Images = {
+
+
+    search,
+    movie_poster,
+    playbutton,
+    searchsvg,
+    };
+
+export default Images;
